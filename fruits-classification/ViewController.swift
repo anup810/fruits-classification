074ae4c50adce817dd77d9c0e4ff3577006633d8
Anup Saud <anupsaud10@gmail.com>
2024-10-25 21:46:19 -0400

    Create simple UI and establish IBOutlet connections

--- fruits-classification/ViewController.swift
+++ fruits-classification/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var classificationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+    }
+    
 }
 
