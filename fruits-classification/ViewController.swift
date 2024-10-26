8bf3ef06794396d5402ae800ccca76845e1c944b
Anup Saud <anupsaud10@gmail.com>
2024-10-25 21:33:28 -0400

    Initial Commit

---
+++ fruits-classification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fruits-classification
+//
+//  Created by Anup Saud on 2024-10-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
