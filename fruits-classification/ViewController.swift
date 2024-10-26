55f3672a676cd7a32a1cbeeeaa7053801eeee338
Anup Saud <anupsaud10@gmail.com>
2024-10-25 22:12:11 -0400

    Add camera and photo library picker with source selection alert

--- fruits-classification/ViewController.swift
+++ fruits-classification/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var classificationLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
@@ -18,6 +18,40 @@ class ViewController: UIViewController {
 
    
     @IBAction func cameraButtonPressed(_ sender: Any) {
+        // check if camera access is given
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        let photoSourcePicker = UIAlertController()
+        
+        let takePhotoAction = UIAlertAction(title: "Take Photo", style: .default) { _ in
+            self.presentPhotoPicker(sourceType: .camera)
+        }
+        
+        let choosePhotoAction = UIAlertAction(title: "Choose Photo", style: .default) { _ in
+            self.presentPhotoPicker(sourceType: .photoLibrary)
+        }
+        
+        photoSourcePicker.addAction(takePhotoAction)
+        photoSourcePicker.addAction(choosePhotoAction)
+        photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(photoSourcePicker, animated: true, completion: nil)
+    }
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        imageView.image = image
+        
     }
     
 }
